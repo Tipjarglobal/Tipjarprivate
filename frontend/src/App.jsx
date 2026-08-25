2f70c162b3d2d737545379b593ab37a68f5a1a76
Tipjar Dev <dev@tipjar.global>
2026-08-25 02:28:38 +0200

    fix: rename Raster6_8Lang -> Raster6_BLang, fix whitescreen, 2347 modules

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,16 @@ import Raster3_AiPicks from "./components/Raster3_AiPicks.jsx";
 import Raster4_Money from "./components/Raster4_Money.jsx";
 import Raster4b_CommunityLive from "./components/Raster4b_CommunityLive.jsx";
 import Raster5_InputFeedback from "./components/Raster5_InputFeedback.jsx";
-import Raster6_8Lang from "./components/Raster6_8Lang.jsx";
+import Raster6_BLang from "./components/Raster6_BLang.jsx";
+
+class Err extends React.Component{
+  state={e:null}
+  static getDerivedStateFromError(e){return {e}}
+  render(){
+    if(this.state.e) return <div style={{background:'white', color:'red', padding:'20px', whiteSpace:'pre-wrap', fontSize:'14px', minHeight:'100vh'}}><b>CRASH in {this.props.name}:</b>{String(this.state.e)}<br/><br/>{this.state.e?.stack}</div>
+    return this.props.children
+  }
+}
 
 export default function App(){
   const [lang, setLang] = useState(()=> localStorage.getItem("tj_lang") || "de");
@@ -18,14 +27,14 @@ export default function App(){
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-black text-white">
- <Raster1_RentPills lang={lang} setLang={setLang} />
- <Raster2_Header lang={lang} setLang={setLang} />
- <Raster2_Supporter lang={lang} setLang={setLang} />
- <Raster3_AiPicks lang={lang} setLang={setLang} />
- <Raster4_Money lang={lang} setLang={setLang} />
- <Raster4b_CommunityLive lang={lang} setLang={setLang} />
- <Raster5_InputFeedback lang={lang} setLang={setLang} />
- <Raster6_8Lang lang={lang} setLang={setLang} />
+        <Err name="Raster1_RentPills"><Raster1_RentPills lang={lang} setLang={setLang} /></Err>
+        <Err name="Raster2_Header"><Raster2_Header lang={lang} setLang={setLang} /></Err>
+        <Err name="Raster2_Supporter"><Raster2_Supporter lang={lang} setLang={setLang} /></Err>
+        <Err name="Raster3_AiPicks"><Raster3_AiPicks lang={lang} setLang={setLang} /></Err>
+        <Err name="Raster4_Money"><Raster4_Money lang={lang} setLang={setLang} /></Err>
+        <Err name="Raster4b_CommunityLive"><Raster4b_CommunityLive lang={lang} setLang={setLang} /></Err>
+        <Err name="Raster5_InputFeedback"><Raster5_InputFeedback lang={lang} setLang={setLang} /></Err>
+        <Err name="Raster6_BLang"><Raster6_BLang lang={lang} setLang={setLang} /></Err>
       </div>
       <Toaster />
     </BrowserRouter>
